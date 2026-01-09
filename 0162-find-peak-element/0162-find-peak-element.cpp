b279ded4579b49c864aa9273baafa666fa681024
Sampad Roy <sampadroy391@gmail.com>
2026-01-09 19:54:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& a) {
+        int n=a.size();
+        int srt=0;
+        int end=n-1;
+        while(srt<=end)
+        {
+            int mid=(srt+end)/2;
+            if(mid==0) 
+            {
+                if(a[mid]>a[mid+1]) return mid;
+                else srt=mid+1;
+            }
+            else if(mid==a.size()-1)
+            {
+                if(a[mid]>a[mid-1]) return mid;
+                else
+                {
+                    end=mid-1;
+                }
+            }
+            else if(a[mid]>a[mid+1] && a[mid]>a[mid-1])
+            {
+                return mid;
+            }
+            else
+            {
+                if(a[mid]>a[mid-1]) 
+                {
+                   srt=mid+1;
+                }
+                else
+                {
+                    end=mid;
+                }
+            }
+
+        }
+        return 0;
+    }
+};
