e07cbcf61237c0b29636fae90c602897ebeebb1c
Sampad Roy <sampadroy391@gmail.com>
2026-01-28 23:00:21 +0530

    Time: 0 ms (100%), Space: 8.3 MB (94.49%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.cpp
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.cpp
@@ -1,37 +1,58 @@
 class Solution {
 public:
-
-    bool check(vector<int>&c)
-    {
-
-        for(auto u:c)
-        {
-            if(u==0) return false;
-        }
-        return true;
-    }
-
-
     int countVowelSubstrings(string word) {
-
         int n=word.size();
-        int total=0;
-        for(int i=0; i<n; i++)
-        {
+        int l=0,r=0,ans=0,start=0;
 
-            vector<int>c(5,0);
-            for(int j=i; j<n; j++)
-            {
-              if(word[j]=='a') c[0]++;
-              else if(word[j]=='e') c[1]++;
-              else if(word[j]=='i') c[2]++;
-              else if(word[j]=='o') c[3]++;
-              else if(word[j]=='u') c[4]++;
-              else break;
-              if(check(c)) total++;
-            }
-        }
+        int unique=0;
+        int freq[5]={0};
         
-        return total;
+
+        for(int r=0; r<n; r++)
+        {
+           char x=word[r];
+           if(x!='a' && x!='e' && x!='i' && x!='o' && x!='u')
+           {
+             for(int i=0; i<5; i++)
+             {
+                freq[i]=0;
+             }
+             unique=0;
+             l=r+1;
+             start=r+1;
+             continue;
+           }
+
+
+           //if find
+           if(x=='a' && freq[0]++==0) unique++;
+           if(x=='e' && freq[1]++==0) unique++;
+           if(x=='i' && freq[2]++==0) unique++;
+           if(x=='o' && freq[3]++==0) unique++;
+           if(x=='u' && freq[4]++==0) unique++;
+
+           while(unique==5)
+           {    
+           //Shink
+           char c=word[l];
+           if(c=='a' && --freq[0]==0) unique--;
+           if(c=='e' && --freq[1]==0) unique--;
+           if(c=='i' && --freq[2]==0) unique--;
+           if(c=='o' && --freq[3]==0) unique--;
+           if(c=='u' && --freq[4]==0) unique--;
+           l++;
+       
+           } 
+
+           ans+=(l-start);  
+    
+
+        }
+
+
+       return ans;
+
+
+
     }
 };
