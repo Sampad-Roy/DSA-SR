3dd2df6773a9b895d72e3be78f838f6bce2d70b8
Sampad Roy <sampadroy391@gmail.com>
2026-01-31 19:39:54 +0530

    Time: 879 ms (12.03%), Space: 160.8 MB (5.42%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,95 +1,99 @@
 class Solution {
 public:
+    int n,m;
 
-   int n,m;
-   vector<vector<int>> d={{1,0},{0,1},{0,-1},{-1,0}};
-   bool find(vector<vector<char>>&a,int i,int j,int idx, string &b)
-   {  
+    vector<vector<int>>d={{-1,0},{1,0},{0,1},{0,-1}};
 
-      if(idx==b.length()) 
-      {
-        return true;
-      }
-
-      if(i<0 || j<0 || i>=n || j>=m || a[i][j]=='#')
-      {
-        return false;
-      }  
-      if(a[i][j]!=b[idx])
-      {
-        return false;
-      }
-
-      char x=a[i][j];
-      a[i][j]='#';     
-      
-      for(auto u:d)
-      {
-        int new_i=i+u[0];
-        int new_j=j+u[1];
-
-        if(find(a,new_i,new_j,idx+1,b))
+    bool find(vector<vector<char>>&a,int i,int j, int idx, string &b)
+    {
+          
+        if(idx==b.length())
         {
+
             return true;
         }
 
-      }
-
-    a[i][j]=x;
-
-    return false;
-   } 
-
-    bool exist(vector<vector<char>>&a, string b) {     
-    n=a.size();
-    m=a[0].size();
-
-    unordered_map<char,int>mp,mp1;
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<m; j++)
-        {
-            mp[a[i][j]]++;
-        }
-    }
-
-
-    for(auto u:b)
-    {
-
-      mp1[u]++;
-
-    }
-
-    for(auto u:b)
-    {
-        if(mp1[u]>mp[u])
+        if(i<0 || j<0 || i>=n || j>=m || a[i][j]=='#')
         {
             return false;
         }
-    }
-    
 
-
-    if(mp1[b[0]]>mp1[b.back()])
-    {
-      reverse(b.begin(),b.end());
-    }
-
-
-
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<m; j++)
+        if(a[i][j]!=b[idx])
         {
-            if(a[i][j]==b[0] && find(a,i,j,0,b))
+            return false;
+        }
+
+        char temp=a[i][j];
+        a[i][j]='#';
+
+
+        for(auto u:d)
+        {
+           int new_i=i+u[0];
+           int new_j=j+u[1];
+
+
+           if(find(a,new_i,new_j,idx+1,b))
+           {
+            return true;
+           }
+
+        }
+
+        a[i][j]=temp;
+        return false;       
+
+    }
+
+
+    bool exist(vector<vector<char>>& board, string word) {
+
+        n=board.size();
+        m=board[0].size();
+        
+        map<char,int>mp,mp1;
+
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
             {
-                return true;
+             
+              mp[board[i][j]]++;
+
             }
         }
-    }
 
-    return false;
+
+        for(auto u:word)
+        {
+            mp1[u]++;
+        }
+
+
+        for(auto u:word)
+        {
+          if(mp1[u]>mp[u])
+          {
+            return false;
+          }
+
+        }
+
+
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(board[i][j]==word[0] && find(board,i,j,0,word))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
         
     }
 };
