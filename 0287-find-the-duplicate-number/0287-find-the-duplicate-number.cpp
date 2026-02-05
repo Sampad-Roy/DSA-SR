52ba49cc27388c141c7df44a25021569f07adae8
Sampad Roy <sampadroy391@gmail.com>
2026-02-06 00:02:03 +0530

    Time: 138 ms (5.01%), Space: 104.6 MB (6.51%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -3,25 +3,17 @@ public:
     int findDuplicate(vector<int>& nums) { 
 
         int n=nums.size();
-        for(int i=0; i<n-1; i++)
-        {
-            if(nums[i]==nums[i+1])
-            {
-                return nums[i];
-            }
-        }
+        sort(nums.begin(),nums.end());
         unordered_map<int,int>mp;
-        for(auto u:nums) mp[u]++;
-        
-        if(mp.size()==1)
+        for(auto u:nums)
         {
-            return nums[0];
-        }
-        for(auto u:mp)
-        {
-            if(u.second>=2) return u.first;
+            mp[u]++;
         }
 
+        for(auto u:mp)
+        {
+          if(u.second>=2) return u.first;
+        }
         return -1;
 
 
