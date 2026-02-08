614df5102122cb54a989639d2963b092b321b230
Sampad Roy <sampadroy391@gmail.com>
2026-02-08 19:36:44 +0530

    Time: 75 ms (7.79%), Space: 55.2 MB (16.62%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -2,22 +2,35 @@ class Solution {
 public:
     int minimumDeletions(string s) {
         int n=s.size();
-        stack<int>st;
-        int cnt=0;
+        vector<int>c_a(n,0),c_b(n,0);
+
+        int cntb=0;
         for(int i=0; i<n; i++)
         {
-          if(!st.empty() && s[i]=='a' && st.top()=='b')
-          {
-                cnt++;
-                st.pop();
-          }
-          else
-          {
-            st.push(s[i]);
-          }
+            c_b[i]=cntb;
+            if(s[i]=='b')
+            {
+                cntb++;
+            }
+        }
+        int cnta=0;
+        for(int i=n-1; i>=0; i--)
+        {
+            c_a[i]=cnta;
+            if(s[i]=='a')
+            {
+                cnta++;
+            }
+        }
+        int mn=1e9;
+        for(int i=0; i<n; i++)
+        {
+          mn=min(mn,c_a[i]+c_b[i]);
         }
 
-        return cnt;
         
+        return mn;
+
+
     }
 };
