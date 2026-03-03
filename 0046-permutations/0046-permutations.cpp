71ab5b657544a9b61aa3ccdf44642aaecb67bd81
Sampad Roy <sampadroy391@gmail.com>
2026-03-03 13:20:58 +0530

    Time: 0 ms (100%), Space: 10.4 MB (98.06%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
-
-   void get(vector<int>&nums,int idx,vector<vector<int>>&ans)
-   {  
-       if(idx==nums.size())
-       {
-        ans.push_back(nums);
-        return;
-       }
-
-      for(int i=idx; i<nums.size(); i++)
-      {
-        swap(nums[idx],nums[i]);
-        get(nums,idx+1,ans);
-        swap(nums[idx],nums[i]);
-      }
-
-   }
-
-
+  
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>ans;
-        get(nums,0,ans);
-        return ans;
-        
+    vector<vector<int>>ans;
+    sort(nums.begin(),nums.end());
+    ans.push_back(nums);
+
+    while(next_permutation(nums.begin(),nums.end()))
+    {
+        ans.push_back(nums);
+    }      
+
+
+    return ans;
         
     }
 };
