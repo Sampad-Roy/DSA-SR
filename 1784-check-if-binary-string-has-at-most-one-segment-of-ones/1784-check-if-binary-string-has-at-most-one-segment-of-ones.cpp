8cc92eeac6d704c7b580fa0e27649d4ff57fc10c
Sampad Roy <sampadroy391@gmail.com>
2026-03-07 04:18:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,28 +1,8 @@
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-
-      int n=s.size();
-      int i=0;
-      int cnt=0;
-      while(i<n)
-      {
-           if(s[i]=='1')
-           {
-            cnt++;
-            while(s[i]=='1' && i<n)
-            {
-                i++;
-            }
-           }
-           else
-           {
-            i++;
-           }
-      }
-
-      if(cnt>1) return false;
-      else return true;
-        
+         string a="01";
+         if(s.find(a)!=-1) return false;
+         else return true;
     }
 };
