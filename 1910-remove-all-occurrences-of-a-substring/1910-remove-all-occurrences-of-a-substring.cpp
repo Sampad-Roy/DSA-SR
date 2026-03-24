8feb5d977f6b549cbaddfc875eb7e0166c10225c
Sampad Roy <sampadroy391@gmail.com>
2026-03-25 04:46:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        int n=s.size();
-        while(s.size()>0 && s.find(part)<s.size())
-        {
-
-            s.erase(s.find(part),part.size());
-
-        }
-
-        return s;
         
+     while(s.size()>0 && s.find(part)<s.size())
+     {
+        s.erase(s.find(part),part.size());
+     }
+     return s;
     }
 };
