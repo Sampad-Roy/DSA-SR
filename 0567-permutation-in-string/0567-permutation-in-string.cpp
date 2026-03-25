a51bcb65c563071f5f8bfcff47104f1041e9baa7
Sampad Roy <sampadroy391@gmail.com>
2026-03-25 19:25:53 +0530

    Time: 180 ms (23.34%), Space: 9.7 MB (99.91%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
+
     bool check(int fre[],int fre2[])
     {
-
       for(int i=0; i<26; i++)
       {
         if(fre[i]!=fre2[i])
@@ -10,44 +10,39 @@ public:
             return false;
         }
       }
-
       return true;
+
     }
 
 
     bool checkInclusion(string s1, string s2) {
 
-       int fre[26]={0};
-       for(auto u:s1)
-       {
-        fre[u-'a']++;
-       }
-       int r=0;
-       int n=s2.size();
-       int win_size=s1.size();
-       while(r<n)
-       {
-          int win_fre[26]={0};
-          int win_idx=0,idx=r;
-          
-          while(win_idx<win_size && idx<n)
-          {
-            win_fre[s2[idx]-'a']++;
-            idx++,win_idx++;
-          }
+        int n1=s1.size();
+        int n2=s2.size();
+        int fre[26]={0};
+        for(int i=0; i<n1; i++)
+        {
+            fre[s1[i]-'a']++;
+        }
+    
+        for(int i=0; i<n2; i++)
+        {  
+           int fre2[26]={0}; 
+           int winidx=0,idx=i;
+           while(winidx<n1 && idx<n2)
+           {
+            fre2[s2[idx]-'a']++;
+            winidx++,idx++;
+           }
 
-          if(check(fre,win_fre))
-          {
-            return true;
-          }
 
-          r++;
+           if(check(fre,fre2))
+           {
+             return true;
+           }
 
-        
-       }
-       
-       return false;
-
+        }
+        return false;
         
     }
 };
