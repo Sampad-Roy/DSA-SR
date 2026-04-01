e866a2555f638f93a849e8ec236d2715cc10d8bf
Sampad Roy <sampadroy391@gmail.com>
2026-04-01 22:11:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-    
 
-    int n=strs.size();
     sort(strs.begin(),strs.end());
-    string s=strs[0],s1=strs[n-1];
-    string ans="";
-    for(int i=0; i<s.size(); i++)
+    int n=strs.size();
+    string s="";
+    string s1=strs[0],s2=strs[n-1];
+    for(int i=0; i<s1.size(); i++)
     {
-        if(s[i]==s1[i])
+        if(s1[i]==s2[i])
         {
-            ans+=s[i];
+            s+=s1[i];
         }
         else
         {
@@ -19,7 +18,7 @@ public:
         }
     }
 
-    return ans;
+    return s;
 
         
     }
