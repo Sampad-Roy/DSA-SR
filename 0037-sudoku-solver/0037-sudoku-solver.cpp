1881bd141425961d0730dc5e79f7aacdb5f706f7
Sampad Roy <sampadroy391@gmail.com>
2026-04-06 05:13:58 +0530

    Time: 231 ms (74.9%), Space: 8.7 MB (93.45%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,85 +1,95 @@
 class Solution {
 public:
-
-    bool issafe(vector<vector<char>>&board,int row,int col,char dig)
-    {
-       //H
-       for(int j=0; j<9; j++)
-       {
-        if(board[row][j]==dig)
+   bool issafe(vector<vector<char>>&board,int row,int col,int dig)
+   {
+     //H
+     
+     for(int i=0; i<9; i++)
+     {
+        if(board[row][i]==dig)
         {
             return false;
         }
-       }
+     }
 
-       //V
+     //V
 
-
-       for(int i=0; i<9; i++)
-       {
-        if(board[i][col]==dig)
+     for(int j=0; j<9; j++)
+     {
+        if(board[j][col]==dig)
         {
             return false;
         }
-       }
+     }
 
 
-       //Grid
-       int srow=(row/3)*3;
-       int scol=(col/3)*3;
-       for(int i=srow; i<=srow+2; i++)
-       {
-        for(int j=scol; j<=scol+2; j++)
+     int srt_r=(row/3)*3;
+     int srt_c=(col/3)*3;
+
+     for(int k=srt_r; k<=srt_r+2; k++)
+     {
+        for(int k1=srt_c; k1<=srt_c+2; k1++)
         {
-            if(board[i][j]==dig)
+            if(board[k][k1]==dig)
             {
                 return false;
             }
         }
-       }
+     }
 
-       return true;
 
-    }
 
-    
+     return true;
+
+
+   }
+   
+
     bool helper(vector<vector<char>>&board,int row,int col)
-    {   
-       if(row==9) return true;
-       int nrow=row,ncol=col+1;
-       if(ncol==9)
-       {
-        nrow=row+1;
-        ncol=0;
-       }
-       
-       if(board[row][col] != '.')
-       {
-            return helper(board,nrow,ncol);
-       }
-
-      for(char i='1'; i<='9'; i++)
-      {
-        if(issafe(board,row,col,i))
+    {
+        
+        if(row==9)
         {
-            board[row][col]=i;
-            if(helper(board,nrow,ncol))
-            {
-                return true;
-            }  
-
-            board[row][col]='.';
+            return true;
         }
-      }
+        int nxtr=row,nxtc=col+1;
+        if(nxtc==9)
+        {
+            nxtr=row+1;
+            nxtc=0;
+        }
 
-       return false;
+        if(board[row][col]!='.')
+        {
+            return helper(board,nxtr,nxtc);
+        }
+
+        for(char dig='1'; dig<='9'; dig++)
+        {
+            if(issafe(board,row,col,dig))
+            {
+                board[row][col]=dig;
+                if(helper(board,nxtr,nxtc))
+                {
+                    return true;
+                }
+                board[row][col]='.';
+            }
+        }
+
+        return false;
+
+
+        return false;
+
+
+
     }
 
-    
-    void solveSudoku(vector<vector<char>>& board) {  
+    void solveSudoku(vector<vector<char>>& board) { 
 
-     helper(board,0,0);
 
-         
+        helper(board,0,0);
+        
     }
 };
