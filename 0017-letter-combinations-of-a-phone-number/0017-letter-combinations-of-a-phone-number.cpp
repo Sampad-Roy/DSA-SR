0baad33a249bd8e01f19da83ce272e9bc99cef51
Sampad Roy <sampadroy391@gmail.com>
2026-05-20 19:12:34 +0530

    Time: 2 ms (15.76%), Space: 9.5 MB (27.58%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,56 +1,51 @@
 class Solution {
 public:
+   vector<string>ans;
+   
+   void solve(int idx,string &dig,string &temp,unordered_map<char,string>&mp)
+   {
+     
+     if(idx==dig.size())
+     {
+        ans.push_back(temp);
+        return;
+     }
+    
+     char ch=dig[idx];
+     string str=mp[ch];
 
-    vector<string>result;
-    void solve(int idx,string &digits,string &temp,unordered_map<char,string>&mp)
-    {
-             
-            if(idx >= digits.size())
-            {
-                result.push_back(temp);
-                return;
-            }
-
-
-            char ch=digits[idx];
-            string str=mp[ch];
-
-            for(int i=0; i<str.size(); i++)
-            {
-                //do
-                temp.push_back(str[i]);
-                solve(idx+1,digits,temp,mp);
-                temp.pop_back();
-            }
-
-    }
+     for(int i=0; i<str.size(); i++)
+     {
+       temp.push_back(str[i]);
+       solve(idx+1,dig,temp,mp);
+       temp.pop_back();
+     }
+    
+   }
 
 
     vector<string> letterCombinations(string digits) {
-    
-    if(digits.size()==0)
-    {
-        return {};
-    }
-    
-   
-
-    unordered_map<char,string>mp;
-    mp['2']="abc";
-    mp['3']="def";
-    mp['4']="ghi";
-    mp['5']="jkl";
-    mp['6']="mno";
-    mp['7']="pqrs";
-    mp['8']="tuv";
-    mp['9']="wxyz";
-    
-    string temp="";
-    solve(0,digits,temp,mp);
-    return result;
-
-
+        if(digits.size()==0)
+        {
+            return {};
+        }
         
         
+        unordered_map<char,string>mp;
+        mp['2']="abc";
+        mp['3']="def";
+        mp['4']="ghi";
+        mp['5']="jkl";
+        mp['6']="mno";
+        mp['7']="pqrs";
+        mp['8']="tuv";
+        mp['9']="wxyz";
+        string temp="";
+        solve(0,digits,temp,mp);
+        return ans;
+
+        
+
+
     }
 };
