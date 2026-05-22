f980cf90ea32b9bec6ad122c3bb3f384af428da3
Sampad Roy <sampadroy391@gmail.com>
2026-05-23 04:47:51 +0530

    Time: 2326 ms (11.18%), Space: 462.8 MB (9.17%) - LeetHub

---
+++ 3923-minimum-generations-to-target-point/3923-minimum-generations-to-target-point.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    int minGenerations(vector<vector<int>>& points, vector<int>& target) {
+    
+
+    int n=points.size();
+    set<vector<int>>all;
+
+    for(int i=0; i<n; i++)
+    {
+        if(points[i]==target)
+        {
+            return 0;
+        }
+        all.insert(points[i]);
+    }
+
+    int gen=1;
+    while(1)
+    {
+        
+
+        vector<vector<int>>curr(all.begin(),all.end());
+        vector<vector<int>>neww;
+
+        int m=curr.size();
+        
+        for(int i=0; i<m-1; i++)
+        {
+            for(int j=i+1; j<m; j++)
+            {
+                vector<int>a=curr[i];
+                vector<int>b=curr[j];
+
+                vector<int>c={
+                (a[0]+b[0])/2,
+                (a[1]+b[1])/2,
+                (a[2]+b[2])/2 
+                };
+
+
+                if(!all.count(c))
+                {
+                    if(c==target)
+                    {
+                       return gen;
+                    }
+                    neww.push_back(c);
+                }
+            }
+            
+        }
+
+
+        if(neww.empty()) break;
+
+        for(auto &u:neww)
+        {
+           all.insert(u);
+        }
+
+        gen++;
+
+
+    }
+
+
+   return -1;
+
+
+
+        
+    }
+};
