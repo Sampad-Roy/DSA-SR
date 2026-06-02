a4e5e6b6749624a158abb6041677687bcd56bda9
Sampad Roy <sampadroy391@gmail.com>
2026-06-02 13:57:45 +0530

    Time: 2 ms (71.73%), Space: 91.7 MB (85.65%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
+    int solve(int n, int m,vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration)
+    {
+      int x=INT_MAX;
+      for(int i=0; i<n; i++)
+      {
+        x=min(x,landStartTime[i]+landDuration[i]);
+      }
+      int y=INT_MAX;
+      for(int i=0; i<m; i++)
+      {
+        int srt=max(x,waterStartTime[i]);
+        y=min(y,srt+waterDuration[i]);
+      }
+
+      return y;
+
+
+    }
+
+
+
     int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
 
         int n=landStartTime.size();
         int m=waterStartTime.size();
 
-        
-        int ans=1e9;
-        for(int i=0; i<n; i++)
-        {   
-           for(int j=0; j<m; j++)
-           {
-            int srt1=landStartTime[i];
-            int duration1=srt1+landDuration[i];
-            int w_srt=max(duration1,waterStartTime[j]);
-            ans=min(ans,w_srt+waterDuration[j]);
+        int ans=0;
+        //l->w
+        int x=solve(n,m,landStartTime,landDuration,waterStartTime,waterDuration);
+        int y=solve(m,n,waterStartTime,waterDuration,landStartTime,landDuration);
 
-            int srt2=waterStartTime[j];
-            int duration2=srt2+waterDuration[j];
-            int l_srt=max(duration2,landStartTime[i]);
-            ans=min(ans,l_srt+landDuration[i]);
-
-           }
-        }
-
-        return ans;
-
+        return min(x,y);
         
     }
 };
