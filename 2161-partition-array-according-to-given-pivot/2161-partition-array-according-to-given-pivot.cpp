db19c1edcdeb272c9992cad22f3eff465bff25e3
Sampad Roy <sampadroy391@gmail.com>
2026-06-08 11:24:15 +0530

    Time: 13 ms (15.93%), Space: 133.4 MB (62.93%) - LeetHub

---
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+
+        int n=nums.size();
+
+
+        vector<int>ans;
+        int x=0;
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i]<pivot)
+            {
+                ans.push_back(nums[i]);
+            }
+        }
+
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i]==pivot)
+            {
+                ans.push_back(nums[i]);
+
+            }
+        }
+
+
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i]>pivot)
+            {
+                ans.push_back(nums[i]);
+            }
+        }
+
+
+
+        return ans;
+
+        
+    }
+};
