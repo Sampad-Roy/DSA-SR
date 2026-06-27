c0a9c1e9ee3e8e19a962adec12ca74f6c44e7748
Sampad Roy <sampadroy391@gmail.com>
2026-06-27 20:21:09 +0530

    Time: 83 ms (44.71%), Space: 114.8 MB (40.15%) - LeetHub

--- 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
@@ -1,41 +1,30 @@
 class Solution {
 public:
     long long countMajoritySubarrays(vector<int>& nums, int target) {
-        int n = nums.size();
-        long long cnt = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == target) nums[i] = 1;
-            else nums[i] = -1;
-        }
-
-        vector<int> pref(n);
-        pref[0] = nums[0];
-
-        for (int i = 1; i < n; i++) {
-            pref[i] = pref[i - 1] + nums[i];
-        }
-
-        int shift = n;
-        vector<int> freq(2 * n + 1, 0);
-
-        freq[shift] = 1;
-
-        long long valid = 0;
-        int lastSum = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (pref[i] > lastSum) {
-                valid += freq[lastSum + shift];
-            } else {
-                valid -= freq[pref[i] + shift];
+        unordered_map<int,int>mp;
+        int n=nums.size();
+        long long cu_sum=0,valid=0,result=0;
+        mp[0]=1;
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i]==target)
+            {
+              valid+=mp[cu_sum];
+              cu_sum+=1;
+            }
+            else
+            {  
+                cu_sum-=1;
+                valid-=mp[cu_sum];
             }
 
-            cnt += valid;
-            freq[pref[i] + shift]++;
-            lastSum = pref[i];
+            mp[cu_sum]+=1;
+            result+=valid;
         }
 
-        return cnt;
+
+        return result;
+        
     }
 };
