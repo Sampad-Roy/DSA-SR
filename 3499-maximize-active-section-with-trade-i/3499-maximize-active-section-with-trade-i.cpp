f22acddb176a7436120499f0bf5ddfdc956a0732
Sampad Roy <sampadroy391@gmail.com>
2026-07-22 16:27:03 +0530

    Time: 117 ms (25.45%), Space: 80.9 MB (33.93%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -1,31 +1,44 @@
 class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
-        int n = s.length();
+        
+
+        int n=s.size();
+
+        int cnt_1=0;
+        for(auto u:s)
+        {
+            if(u=='1') cnt_1++;
+        }
 
 
-        int activeCount = count(begin(s), end(s), '1');
+        vector<int>c_zero;
 
-        vector<int> inactiveBlocks;
-        int i = 0;
-        while(i < n) {
-            if(s[i] == '0') {
-                int start = i;
-                while(i < n && s[i] == '0') i++;
-
-                inactiveBlocks.push_back(i-start);
-            } else {
+        int i=0;
+        while(i<n)
+        {
+          if(s[i]=='0')
+          { 
+            int srt=i;
+            while(i<n && s[i]=='0')
+            {
                 i++;
             }
+            c_zero.push_back(i-srt);
+          }
+          else
+          {
+            i++;
+          }
         }
 
-        int maxPairSum = 0;
-        
-        for(int i = 1; i < inactiveBlocks.size(); i++) {
-            maxPairSum = max(maxPairSum, inactiveBlocks[i] + inactiveBlocks[i-1]);
+        int mx=0;
+        for(int i=1; i<c_zero.size(); i++)
+        {
+            mx=max(mx,c_zero[i]+c_zero[i-1]);
         }
 
-        return maxPairSum + activeCount;
-        
+        return mx+cnt_1;
+
     }
 };
