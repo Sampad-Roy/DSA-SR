dec644fb16460242ea0ddb3312f51c2c7d4dc8aa
Sampad Roy <sampadroy391@gmail.com>
2026-07-25 00:48:46 +0530

    Time: 224 ms (96.07%), Space: 35.7 MB (77.53%) - LeetHub

---
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+
+        int pairXor[2048]={};
+        int tripleXor[2048]={};
+
+        for (int i=0; i<n; i++) 
+        {
+            for (int j=i; j<n; j++) 
+            {
+                pairXor[nums[i]^nums[j]] = 1;
+            }
+        }
+
+        for (int i=0; i<n; i++) 
+        {
+            for (int j=0; j<2048; j++) 
+            {
+                if (pairXor[j]==1) 
+                {
+                    tripleXor[j^nums[i]]=1;
+                }
+            }
+        }
+
+        
+        int cnt=0;
+        for (int value:tripleXor) 
+        {
+            if (value==1) 
+            {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+
+        
+};
