d84637b295278e5ea04d5081b3bc1f12758a5505
Sampad Roy <sampadroy391@gmail.com>
2026-09-24 11:38:25 +0530

    Time: 215 ms (11.11%), Space: 172.3 MB (7.34%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,26 +1,41 @@
 class Solution {
 public:
-    int minOperations(vector<int>& nums, int x) {        
-        int n    = nums.size();
-        int sum  = 0;
-        unordered_map<int, int> mp;
-        mp[0] = -1;
-        for(int i = 0; i<n; i++) {
-            sum += nums[i];
-            mp[sum] = i;
+    int minOperations(vector<int>& nums, int x) {
+        
+        int n=nums.size();
+        int sum=0;
+        unordered_map<int,int>mp;
+        mp[0]=-1;
+        for(int i=0; i<n; i++)
+        {
+           sum+=nums[i];
+           mp[sum]=i;
         }
-        if(sum < x)
+        if(sum<x)
+        {
             return -1;
-        int restSum  = sum-x;
-        int longest  = INT_MIN;
-        sum = 0;
-        for(int i = 0; i<n; i++) {
-            sum += nums[i];
-            if(mp.count(sum-restSum)) {
-                longest = max(longest, i-mp[sum-restSum]);
+        }
+
+        int rest=sum-x;
+        int longest=INT_MIN;
+
+        sum=0;
+        for(int i=0; i<n; i++)
+        {
+            sum+=nums[i];
+            if(mp.count(sum-rest))
+            {
+               longest=max(longest,i-mp[sum-rest]);
             }
         }
+
+        if(longest!=INT_MIN)
+        {
+            return (int) n-longest;
+        }
         
-        return longest==INT_MIN?-1:n-longest;
+        return -1;
+
+
     }
 };
